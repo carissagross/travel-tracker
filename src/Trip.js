ee1ad464cecd8eab9f605126159f981230bdebd9
Carissa Gross <carissamgross@gmail.com>
2022-09-11 13:53:50 -0600

    Create Trip class

---
+++ src/Trip.js
@@ -0,0 +1,14 @@
+class Trip {
+    constructor(id, userID, destinationID, travelers, date, duration, status, suggestedActivities) {
+        this.id = id;
+        this.userID = userID;
+        this.destinationID = destinationID;
+        this.travelers = travelers;
+        this.date = date;
+        this.duration = duration;
+        this.status = status;
+        this.suggestedActivities = suggestedActivities;
+    }
+}
+
+export default Trip;
